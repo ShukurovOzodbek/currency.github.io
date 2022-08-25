1f381840efac3cc9beb758d4a0298e96559d4fa9
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich.com>
2022-08-26 09:24:01 +1300

    edited

--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -6,7 +6,6 @@ const Market = () => {
     
     return (
         <div style={{width: '300px', height: '300px'}}>
-            <LineChart w="100%" h="100%" arr={[2,4,6,8,10,12,14,16]} arr2={[28, 21, 40, 35, 43, 43, 45, 48]}/>
         </div>
     )
 }
